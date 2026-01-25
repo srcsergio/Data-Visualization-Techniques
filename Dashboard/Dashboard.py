7a1b0c55d8a7ac39c5f29b773d110bbece3c85dd
srcsergio <163417448+srcsergio@users.noreply.github.com>
2026-01-25 12:40:02 +0000

    Dashoborad Update

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -7,17 +7,21 @@ st.set_page_config(
 )
 
 
-st.write("# Dashboard The Fremont Bridge Bicycle Counter! 🚦")
+st.write("#### Dashboard The Fremont Bridge Bicycle Counter! 🚦")
 
 st.sidebar.success("Select a option above.")
 
 @st.cache_data
 def load_dataset():
 
-    df = pd.read_csv('df_fremont.csv')
+    df = pd.read_csv('Fremont_Bridge_Bicycle_Counter.csv')
     df['Date'] = pd.to_datetime(df['Date'])
     df.set_index('Date', inplace=True)
+    df.rename({'Fremont Bridge Sidewalks, south of N 34th St Total' : 'Total'}, axis=1, inplace=True)
+    df.rename({'Fremont Bridge Sidewalks, south of N 34th St Cyclist West Sidewalk' : 'West Sidewalk'}, axis=1, inplace=True)
+    df.rename({'Fremont Bridge Sidewalks, south of N 34th St Cyclist East Sidewalk' : 'East Sidewalk'}, axis=1, inplace=True)
     return df
 
 df = load_dataset()
 st.dataframe(df)
+
