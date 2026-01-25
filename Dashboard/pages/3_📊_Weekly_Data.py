bd2f00416652df64fbe5f670dbe3c481d7655406
srcsergio <163417448+srcsergio@users.noreply.github.com>
2026-01-25 15:14:56 +0000

    update Weeky and Daily plots

--- Dashboard/pages/3_📊_Weekly_Data.py
+++ Dashboard/pages/3_📊_Weekly_Data.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-from urllib.error import URLError
+
 
 st.set_page_config(page_title='Weekly Data', page_icon='📉')
 
@@ -14,3 +14,27 @@ st.write(
 """
 )
 
+@st.cache_data
+def load_dataset():
+
+    df = pd.read_csv('df_fremont.csv')
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+
+    return df
+
+df = load_dataset()
+weekly_total = df['Total'].resample('W').sum()
+dfweekly = pd.DataFrame({'total': weekly_total})
+
+left, right = st.columns(2, border=True)
+
+
+left.markdown("Total by weekly",text_alignment="center")
+left.bar_chart(dfweekly, x_label='month', y_label='Total bicycle', color= "#ffaa0088", stack=False)
+
+right.markdown("Total by weekly",text_alignment="center")
+right.line_chart(dfweekly)
+
+
+st.dataframe(dfweekly)
--- Dashboard/pages/4_📊_Daily_Data.py
+++ Dashboard/pages/4_📊_Daily_Data.py
@@ -1,16 +1,33 @@
 import streamlit as st
 import pandas as pd
-import altair as alt
-from urllib.error import URLError
 
-st.set_page_config(page_title='Daily Data', page_icon='📉')
 
-st.markdown("# Daily Data")
+st.set_page_config(page_title='Daily Data')
+
+st.markdown("### Daily Data")
 st.sidebar.success('Daily Data')
 
-st.write(
-        """
-        Daily
-"""
-)
+def load_dataset():
+    df = pd.read_csv('df_fremont.csv')
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df['time'] = df.index.time
 
+    return df
+
+df = load_dataset()
+
+by_tyme_day = df.groupby('hour')['Total'].mean()
+by_tyme = df.groupby(df.index.time)['Total'].mean()
+
+
+left, right = st.columns(2, border=True)
+
+left.markdown("Average hourly bicycle", text_alignment='center')
+left.bar_chart(by_tyme_day, x_label='hour', y_label='Average hourly')
+
+right.markdown("Average hourly bicycle",text_alignment="center")
+right.line_chart(by_tyme_day, x_label='hour', y_label='Average hourly')
+
+df_by_time_day= pd.DataFrame({'Average by hour': by_tyme})
+st.write(df_by_time_day)
